3dcb98a856ce4b9a6f2a42b8811ec283be11a077
Tezcat5 <vicpero96@gmail.com>
2024-02-28 10:17:21 +0100

    GroundSensor

---
+++ Assets/Assets/Scripts/GroundSensor1.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    public bool isGrounded;
+
+    PlayerMovement playerScript;
+
+    void Awake()
+    {
+        playerScript =GetComponentInParent<PlayerMovement>();
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        
+        
+        isGrounded = true;
+        
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+
+    {
+        isGrounded = false;
+    }
+
+}
